dceaf561761be2f0cffa3cf8afc6d092135cbc1b
Otto Jongerius <otto.jongerius@trovemoney.co.nz>
2021-08-11 14:57:12 +1200

    fix(foo): push bundle out to allow tesing using yarn / npm

---
+++ dist/bundle.d.ts
@@ -0,0 +1,40 @@
+// epsagon.d.ts
+declare module 'epsagon' {
+  import 'aws-lambda'
+
+  export function init(options: {
+    token: string
+    appName: string
+    metadataOnly?: boolean
+    useSSL?: boolean
+    traceCollectorURL?: string
+    isEpsagonDisabled?: boolean
+    urlPatternsToIgnore?: string[]
+    ignoredKeys?: (string | RegExp)[]
+    ignoredDBTables?: (string | RegExp)[]
+    labels?: string[][]
+    sendOnlyErrors?: boolean
+    sendTimeout?: number
+    decodeHTTP?: boolean
+    disableHttpResponseBodyCapture?: boolean
+    loggingTracingEnabled?: boolean
+    sendBatch?: boolean
+    maxTraceWait?: number
+    batchSize?: number
+    maxBatchSizeBytes?: number
+  }): void
+  export function label(key: string, value: string): void
+  export function unpatch(): void
+  export function setError(error: Error): void
+  export function setWarning(error: Error): void
+  export function getTraceUrl(): string
+  export function lambdaWrapper<T extends Function>(f: T): T
+  export function stepLambdaWrapper<T extends Function>(f: T): T
+  export function openWhiskWrapper<T extends Function>(f: T): T
+  export function nodeWrapper<T extends Function>(f: T): T
+  export function tencentFunctionWrapper<T extends Function>(f: T): T
+  export function googleCloudFunctionWrapper<T extends Function>(f: T): T
+  export function wrapBatchJob(): void
+  export function enable(): void
+  export function disable(): void
+}
